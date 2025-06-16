90acec26e4371a8bcc6c46dee8422a027fd3ac62
guru963 <gururaman2006@gmail.com>
2025-06-16 19:53:17 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,20 @@
+const c=document.querySelector('#im1');
+c.addEventListener('click',()=>{
+    const d=document.querySelector('.zoomed');
+    d.src="image/red_shoes2.png"
+})
+const r=document.querySelector('#im2');
+r.addEventListener('click',()=>{
+    const d=document.querySelector('.zoomed');
+    d.src="image/red_shoes1.png"
+})
+const t=document.querySelector('#im3');
+t.addEventListener('click',()=>{
+    const d=document.querySelector('.zoomed');
+    d.src="image/red_shoes3.png"
+})
+const y=document.querySelector('#im4');
+y.addEventListener('click',()=>{
+    const d=document.querySelector('.zoomed');
+    d.src="image/red_shoes4.png"
+})
